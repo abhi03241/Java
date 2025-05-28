8b8bcd7f4889c1db844f36bd4e2ff3a309d2ca80
Abhishek Shukla <abhishek.shukladavv@gmail.com>
2025-05-28 22:59:10 +0530

    optimized

--- core/String and it mehord/AreaCircle.java
+++ core/String and it mehord/AreaCircle.java
@@ -1,28 +1,52 @@
+/*optimized  */
 class Circle {
-
     double r;
-    
+
     public Circle(double r) {
         this.r = r;
     }
-    public double areaCircle()
-    {
-        return  (Math.pow(this.r, 2)) * Math.PI ;
-        
+
+    public double areaCircle() {
+        return r * r * Math.PI;
     }
-    public void show(){
-        System.out.println(areaCircle());
+
+    public void show() {
+        System.out.println("Area: " + areaCircle());
     }
 }
 
 public class AreaCircle {
     public static void main(String[] args) {
         Circle c1 = new Circle(5);
-        // System.out.println(c1);
-        c1.areaCircle();
-        c1.show();
+        c1.show(); // Clean, simple, and safe
     }
 }
+
+// class Circle {
+
+//     double r;
+    
+//     public Circle(double r) {
+//         this.r = r;
+//     }
+//     public double areaCircle()
+//     {
+//         return  (Math.pow(this.r, 2)) * Math.PI ;
+        
+//     }
+//     public void show(){
+//         System.out.println(areaCircle());
+//     }
+// }
+
+// public class AreaCircle {
+//     public static void main(String[] args) {
+//         Circle c1 = new Circle(5);
+//         // System.out.println(c1);
+//         c1.areaCircle();
+//         c1.show();
+//     }
+// }
 
 // class Circle {
 
