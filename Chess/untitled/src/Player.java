5665b19f3e5f1ad6d51e17e48237e41e5afa82bf
abhi03241 <abhishek.shukladavv@gmail.com>
2025-10-28 03:40:30 +0530

    Add Chess untitled project

---
+++ Chess/untitled/src/Player.java
@@ -0,0 +1,23 @@
+public abstract class Player {
+    public boolean whiteSide;
+    public boolean humanPlayer;
+
+    public boolean isWhiteSide()
+    {
+        return this.whiteSide;
+    }
+    public boolean isHumanPlayer()
+    {
+        return this.humanPlayer;
+    }
+}
+
+public class HumanPlayer extends Player {
+
+    public HumanPlayer(boolean whiteSide)
+    {
+        this.whiteSide = whiteSide;
+        this.humanPlayer = true;
+    }
+}
+
