b1e8fb6bc61325acf1f78f8f61e924c66dcad7cc
abhi03241 <abhishek.shukladavv@gmail.com>
2025-10-29 23:34:58 +0530

    Initial commit for Gradle Learning project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://jsonplaceholder.typicode.com/posts";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful() && response.body() != null) {
+                System.out.println(response.body().string());
+            } else {
+                System.out.println("Request failed: " + response.code());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
